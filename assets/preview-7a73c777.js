89e7396897cdfac509274e6850468c400f0d9307
aliyahishmail <aliyahishmail@users.noreply.github.com>
2023-09-20 22:20:15 +0000

    deploy: 8cc5847ebf015b0bdbc1ebc5e176852a19318868

--- assets/preview-7a73c777.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-d9c52d00.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-d64e854a.js"),["./DocsRenderer-3PUGWF3O-d64e854a.js","./iframe-d9c52d00.js","./compat.module-2df6b160.js","./preact.module-a8db4ca9.js","./hooks.module-1ffebabd.js","./index-774b9526.js","./index-d475d2ea.js","./_commonjsHelpers-de833af9.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-7a73c777.js.map
